da5441c4edbfd4d915f468982978f61514ebf399
Jade Lynn Masker <hopefulfire@gmail.com>
2020-01-27 13:27:31 -0500

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,26 @@
+class Course
 
+	attr_accessor :title, :schedule, :description
 
+	ALL = []
+
+	def initialize(args={})
+		args.each do |key, value|
+			self.send("#{key}=", value)
+		end
+		save
+	end
+
+	def save
+		ALL << self
+	end
+
+	def self.all
+		ALL
+	end
+
+	def self.reset_all
+		ALL.clear
+	end
+
+end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,7 +15,25 @@ class Scraper
       end
     end
   end
-  
+
+  def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+    @courses = get_page.css(".post")
+  end
+
+  def make_courses
+    get_courses.each do |course|
+      title = course.css("h2").text
+      schedule = course.css(".date").text
+      description = course.css("p").text
+      course = {title: title, schedule: schedule, description: description}
+      Course.new(course)
+    end
+  end
+
 end
 
 
